812936880ccf11370e78b3902f8d93ca94405c21
dltmfekgml <dltmfekgml@naver.com>
2023-04-11 17:41:38 +0900

    feat:HotelRow-숙소 선택시 갯수에 따른 금액 화면에 출력하기

--- gajae/src/pages/login/LoginPage.jsx
+++ gajae/src/pages/login/LoginPage.jsx
@@ -1,14 +1,24 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { Form, Image } from 'react-bootstrap';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
-import HeaderNav1 from '../../components/header/HeaderNav1';
-import HeaderNav2 from '../../components/header/HeaderNav2';
-import { setToastMessage } from '../../redux/toastStatus/action';
-import { googleLogin } from '../../service/authLogic';
-import { signinDB } from '../../service/user/user';
-import { DividerDiv, DividerHr, DividerSpan, MyH1, MyInput, MyLabel, MyP, PwEye, SubmitButton } from '../../style/FormStyle';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Form, Image } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import HeaderNav1 from "../../components/header/HeaderNav1";
+import HeaderNav2 from "../../components/header/HeaderNav2";
+import { setToastMessage } from "../../redux/toastStatus/action";
+import { googleLogin } from "../../service/authLogic";
+import { signinDB } from "../../service/user/user";
+import {
+  DividerDiv,
+  DividerHr,
+  DividerSpan,
+  MyH1,
+  MyInput,
+  MyLabel,
+  MyP,
+  PwEye,
+  SubmitButton,
+} from "../../style/FormStyle";
 /**
  *
  * @returns 로그인 페이지
@@ -19,13 +29,13 @@ const LoginPage = () => {
 
   const userAuth = useSelector((store) => store.userAuth);
   const [tempUser, setTempUser] = useState({
-    user_id: '',
-    user_pw: '',
+    user_id: "",
+    user_pw: "",
   });
 
   const [userId, setUserId] = useState();
 
-  const REDIRECT_URI = 'http://localhost:9999/auth/kakao/callback';
+  const REDIRECT_URI = "http://localhost:9999/auth/kakao/callback";
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   const kakaoLogin = () => {
@@ -44,17 +54,19 @@ const LoginPage = () => {
       console.log(result);
       console.log(result.uid);
       setUserId(result.uid);
-      window.localStorage.setItem('userId', result.uid);
-      navigate('/');
+      window.localStorage.setItem("userId", result.uid);
+      navigate("/");
       window.location.reload();
     } catch (error) {
-      dispatch(setToastMessage(error + ': 로그인과정 중에 문제가 발생했습니다.'));
+      dispatch(
+        setToastMessage(error + ": 로그인과정 중에 문제가 발생했습니다.")
+      );
     }
   };
 
   const signin = async () => {
     if (!tempUser.user_id || !tempUser.user_pw) {
-      dispatch(setToastMessage('아이디와 비밀번호를 입력해주세요.'));
+      dispatch(setToastMessage("아이디와 비밀번호를 입력해주세요."));
     }
 
     const response = await signinDB(tempUser);
@@ -65,33 +77,33 @@ const LoginPage = () => {
 
     console.log(jsonDoc[0]);
     if (response.data) {
-      window.localStorage.setItem('userId', jsonDoc[0].USER_ID);
-      window.localStorage.setItem('userNickname', jsonDoc[0].USER_NICKNAME);
-      window.localStorage.setItem('userAuth', jsonDoc[0].USER_AUTH);
-      window.localStorage.setItem('userEmail', jsonDoc[0].USER_EMAIL);
-      window.localStorage.setItem('userBirth', jsonDoc[0].USER_BIRTH);
-      navigate('/');
+      window.localStorage.setItem("userId", jsonDoc[0].USER_ID);
+      window.localStorage.setItem("userNickname", jsonDoc[0].USER_NICKNAME);
+      window.localStorage.setItem("userAuth", jsonDoc[0].USER_AUTH);
+      window.localStorage.setItem("userEmail", jsonDoc[0].USER_EMAIL);
+      window.localStorage.setItem("userBirth", jsonDoc[0].USER_BIRTH);
+      navigate("/");
     } else {
-      dispatch(setToastMessage('아이디 또는 비밀번호가 일치하지 않습니다.'));
+      dispatch(setToastMessage("아이디 또는 비밀번호가 일치하지 않습니다."));
     }
   };
 
   const [submitButton, setSubmitButton] = useState({
     disabled: true,
-    bgColor: 'rgb(175, 210, 244)',
+    bgColor: "rgb(175, 210, 244)",
     hover: false,
   });
 
   const [passwordType, setPasswordType] = useState({
-    type: 'password',
+    type: "password",
     visible: false,
   });
 
   useEffect(() => {
-    if (tempUser.user_id !== '' && tempUser.user_pw !== '') {
-      setSubmitButton({ disabled: false, bgColor: 'rgb(105, 175, 245)' });
+    if (tempUser.user_id !== "" && tempUser.user_pw !== "") {
+      setSubmitButton({ disabled: false, bgColor: "rgb(105, 175, 245)" });
     } else {
-      setSubmitButton({ disabled: true, bgColor: 'rgb(175, 210, 244)' });
+      setSubmitButton({ disabled: true, bgColor: "rgb(175, 210, 244)" });
     }
   }, [tempUser]);
 
@@ -103,20 +115,28 @@ const LoginPage = () => {
 
   const passwordView = (e) => {
     const id = e.currentTarget.id;
-    if (id === 'password') {
+    if (id === "password") {
       if (!passwordType.visible) {
-        setPasswordType({ ...passwordType, type: 'text', visible: true });
+        setPasswordType({ ...passwordType, type: "text", visible: true });
       } else {
-        setPasswordType({ ...passwordType, type: 'password', visible: false });
+        setPasswordType({ ...passwordType, type: "password", visible: false });
       }
     }
   };
 
   const toggleHover = () => {
     if (submitButton.hover) {
-      setSubmitButton({ ...submitButton, hover: false, bgColor: 'rgb(105, 175, 245)' });
+      setSubmitButton({
+        ...submitButton,
+        hover: false,
+        bgColor: "rgb(105, 175, 245)",
+      });
     } else {
-      setSubmitButton({ ...submitButton, hover: true, bgColor: 'rgb(58, 129, 200)' });
+      setSubmitButton({
+        ...submitButton,
+        hover: true,
+        bgColor: "rgb(58, 129, 200)",
+      });
     }
   };
 
@@ -127,12 +147,17 @@ const LoginPage = () => {
       <Form className="container">
         <MyH1>로그인</MyH1>
         <MyLabel htmlFor="email">
-          {' '}
+          {" "}
           아이디
-          <MyInput type="text" id="user_id" name="user_id" onChange={(e) => changeUser(e)} />
+          <MyInput
+            type="text"
+            id="user_id"
+            name="user_id"
+            onChange={(e) => changeUser(e)}
+          />
         </MyLabel>
         <MyLabel htmlFor="password">
-          {' '}
+          {" "}
           비밀번호
           <MyInput
             type={passwordType.type}
@@ -147,7 +172,7 @@ const LoginPage = () => {
             onClick={(e) => {
               passwordView(e);
             }}
-            style={{ color: `${passwordType.visible ? 'gray' : 'lightgray'}` }}
+            style={{ color: `${passwordType.visible ? "gray" : "lightgray"}` }}
           >
             <PwEye className="fa fa-eye fa-lg"></PwEye>
           </div>
@@ -180,21 +205,33 @@ const LoginPage = () => {
         </a>
         <button onClick={kakaoLogin}>KAKAO</button>
       </div>
-      <MyP style={{ marginTop: '30px' }}>
+      <MyP style={{ marginTop: "30px" }}>
         신규 사용자이신가요?&nbsp;
-        <Link to="/auth/signup" className="text-decoration-none" style={{ color: 'blue' }}>
+        <Link
+          to="/auth/signup"
+          className="text-decoration-none"
+          style={{ color: "blue" }}
+        >
           계정 만들기
         </Link>
       </MyP>
       <MyP>
         아이디를 잊으셨나요?&nbsp;
-        <Link to="/auth/findEmail" className="text-decoration-none" style={{ color: 'blue' }}>
+        <Link
+          to="/auth/findEmail"
+          className="text-decoration-none"
+          style={{ color: "blue" }}
+        >
           아이디 찾기
         </Link>
       </MyP>
       <MyP>
         비밀번호를 잊으셨나요?&nbsp;
-        <Link to="/auth/resetPwd" className="text-decoration-none" style={{ color: 'blue' }}>
+        <Link
+          to="/auth/resetPwd"
+          className="text-decoration-none"
+          style={{ color: "blue" }}
+        >
           비밀번호 찾기
         </Link>
       </MyP>
---
+++ gajae/src/style/HotelStyle.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Charge_title = styled.div`
+  font-family: "KOTRA_GOTHIC";
+  // border: 1px solid black;
+  font-weight: bold;
+  text-align: center;
+  padding: 10px;
+  margin: 10%;
+`;
+
+export const TotalPrice = styled.span`
+  font-family: "KOTRA_GOTHIC";
+  // border: 1px solid black;
+  font-weight: bold;
+  text-align: center;
+  padding: 10px;
+  margin-left: 1em;
+`;
